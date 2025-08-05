02eb64e3407e95e18fe75701b40b89a1db9cef1e
ysm3471 <ysm3471@gmail.com>
2025-08-05 17:05:36 +0900

    test

---
+++ dist/index.d.ts
@@ -0,0 +1,6 @@
+declare const onChangePriceToKorean: (val: number) => string;
+declare const convertPhoneNumber: (number: any) => string;
+declare function checkNum(val: string): string;
+declare function addNum(a: number, b: number): number;
+declare function addNum2(a: number, b: number): number;
+export { addNum, addNum2, checkNum, onChangePriceToKorean, convertPhoneNumber };
